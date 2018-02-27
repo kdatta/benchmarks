391e4c77c116ee7d1fcd9b7c3dd6186d75fe7ff2
kdatta1 <kushal.datta@intel.com>
2018-02-26 23:21:15 -0800

    removed summaries

--- scripts/tf_cnn_benchmarks/train_mcnn_multinode.sh
+++ scripts/tf_cnn_benchmarks/train_mcnn_multinode.sh
@@ -79,8 +79,8 @@ run_instance() {
                         --sync_on_finish=False \
                         --mkl=True \
                         --kmp_affinity=$KMP_AFFINITY \
-                        --summary_verbosity=3 \
-                        --save_summaries_steps=1 \
+                        --summary_verbosity=0 \
+                        --save_summaries_steps=0 \
                         --train_dir='/tmp/tf_cnn_benchmarks/eval' \
                         --ps_hosts=$ps_host \
                         --worker_hosts=$worker_hosts \
